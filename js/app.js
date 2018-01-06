847e96e4cd41b10f7116fb2ba107e16f402e9bec
Alex <jzsplking@gmail.com>
2018-01-06 23:36:37 +0800

    Fix: 之前会出现生成的两个障碍占用同一个位置的bug
    通过修改Entity的initPosition函数来避免此bug
    :

--- js/app.js
+++ js/app.js
@@ -23,6 +23,22 @@ var pavement = (function() {
     return matrix;
 })();
 
+//移除obstacle的函数
+var removeObstacle = function() {
+    if(allObstacles.length === 0) {
+        return;
+    }
+
+    var randomIndex = Math.floor(Math.random() * allObstacles.length);
+
+    //复原pavement中元素
+    var row = allObstacles[randomIndex].y / HEIGHT - 1;
+    var col = allObstacles[randomIndex].x / WIDTH;
+    pavement[row][col] = false;
+
+    allObstacles.splice(randomIndex ,1);
+};
+
 //生成Enemy函数
 function addEnemy(num) {
     for(var i = 0; i < num; i++) {
@@ -77,13 +93,23 @@ var Entity = function() {
 };
 
 Entity.prototype.initPosition = function() {
+    //用来表示宝物跟障碍在节点中的坐标
     var col, row;
-    
+    //随机生成一个坐标并赋值给障碍或宝物的x，y值
 
     col = Math.floor(Math.random() * 5);
     row = Math.floor(Math.random() * 4);
     this.x = WIDTH * col;
     this.y = HEIGHT * (row + 1);
+    //为了不让障碍物或宝物生成在同一地点
+    while(pavement[row][col] === true || (this.x === player.x && this.y === player.y) ) {
+         col = Math.floor(Math.random() * 5);
+         row = Math.floor(Math.random() * 4);
+         this.x = WIDTH * col;
+         this.y = HEIGHT * (row + 1); 
+    }
+    
+    //在pavement记录此坐标
     pavement[row][col] = true;
     console.log(pavement);
 };
@@ -94,7 +120,6 @@ Entity.prototype.initPosition = function() {
 var Obstacle = function() {
     Entity.call(this);
     this.sprite = 'images/Rock.png';
-    this.kind = 'obstacle';
 };
 
 Obstacle.prototype = Object.create(Entity.prototype);
@@ -104,17 +129,40 @@ Obstacle.prototype.render = function() {
      ctx.drawImage(Resources.get(this.sprite), this.x + 20, this.y + 15, 60, 105);
 }
 
+//生成Obstacle函数
+function addObstacle(num) {
+    for(var i = 0; i < num; i++) {
+        var obstacle = new Obstacle();
+        allObstacles.push(obstacle);
+    }
+}
+
+//Treasure类为Entity的子类，当player碰到了treasure,treature消失并触发效果
+var Treasure = function() {
+    Entity.call(this);
+};
+
+Treasure.prototype = Object.create(Entity.prototype);
+Treasure.prototype.constructor = Treasure;
+
+Treasure.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x + 20, this.y + 15, 60, 105);
+}
+
+//Treasure类的子类
+
+
+
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 // 这是我们的玩家
 var Player = function() {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
+    // Player的默认属性，x,y,hp,score,sprite
     this.x = 3 * WIDTH;
     this.y = 5 * HEIGHT;
     this.hp = 100;
     this.score = 0;
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    // player的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/char-boy.png';
 };
 
@@ -130,7 +178,7 @@ Player.prototype.resetPlayer = function() {
         
 };
 
-//处理碰撞时间的函数,判断是否碰撞，50为实验出的经验值
+//处理碰撞的函数,判断是否碰撞，50为实验出的经验值
 function collision(item) {
     if( (item instanceof Enemy) && ( (item.y === player.y) && (Math.abs(item.x - player.x) < 50) )) {
         return true;
@@ -145,6 +193,7 @@ function collision(item) {
     
 }
 
+//处理player与Obstacle碰撞的函数
 function checkCollisionsWithObstacle(array) {
      for (var i = 0; i < array.length; i++) {
         if(collision(array[i])) {
@@ -162,6 +211,7 @@ Player.prototype.checkCollisionsWithEnemy = function(array) {
     }); 
 };
 
+//处理player与Obstacle碰撞的函数，但是不起作用，清探究原因
 Player.prototype.checkCollisionsWithObstacle = function(array) {
     array.forEach(function(element) {
         collision(element);       
@@ -170,8 +220,8 @@ Player.prototype.checkCollisionsWithObstacle = function(array) {
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
 Player.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+    
+    // 当player到达河对岸的逻辑，此处用ScoreFlag来控制其只触发一次
     if(this.y < HEIGHT) {
         this.y = 0;
         if(ScoreFlag === true) {
@@ -184,9 +234,10 @@ Player.prototype.update = function(dt) {
         this.y = 5 * HEIGHT;
     };
 
+    //监控player与Enemy的碰撞
     this.checkCollisionsWithEnemy(allEnemies);
     
-
+    //当player血量低于0，做如下行动
     if(this.hp <= 0 ) {
         alert("game over!");
         this.score = 0;
@@ -194,17 +245,20 @@ Player.prototype.update = function(dt) {
         this.hp = 100;
     }
 
+    //在屏幕显示分数及血量
     $('.hp').html(player.hp);
     $('.score').html(player.score);
     $('.highscore').html(HighScore);
 
+    //如果player走出x的屏幕范围，控制其在屏幕内
     if(this.x < 0) {
         this.x = 0
     }
     else if(this.x > 4 * WIDTH) {
         this.x = 4 * WIDTH;
-    };
+    }
 
+    //更新记录最高分数
     if(HighScore < this.score) {
         HighScore = this.score;
     }
@@ -217,7 +271,7 @@ Player.prototype.render = function() {
 
 //player 键盘控制函数
 Player.prototype.handleInput = function(e) {
-
+    //记录之前的x,y值
     var lastX = this.x,
     lastY = this.y;
 
@@ -234,10 +288,14 @@ Player.prototype.handleInput = function(e) {
         case 'down':
             this.y += HEIGHT;
             break;
+        case 'space':
+            removeObstacle();
+            break;
         default:
             return;
     }
 
+    //如果碰撞到obstacle，返回之前记录值，相当于不能移动
     if (checkCollisionsWithObstacle(allObstacles)) {
         this.x = lastX;
         this.y = lastY
@@ -248,16 +306,12 @@ Player.prototype.handleInput = function(e) {
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-var e1 = new Enemy();
-e1.speed = 0;
+var player = new Player();
 var allEnemies = [];
 var allObstacles = [];
-var o1 = new Obstacle();
-var o2 = new Obstacle();
-allObstacles.push(o1);
-allObstacles.push(o2);
+addObstacle(4);
 addEnemy(5);
-var player = new Player();
+
 
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
@@ -267,7 +321,8 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        32: 'space'
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
